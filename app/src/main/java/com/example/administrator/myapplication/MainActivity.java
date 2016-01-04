8871ccbf3a4e829a0f0eff151910c064de809049
Alexiszou <Alexis.zou@outlook.com>
2016-01-04 19:13:54 +0800

    version 1.1

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initDatas(){
-
+        Log.d(TAG, "initDatas");
     }
 
     @Override
